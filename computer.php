e5b36450cfce4c0a26563df3bcab7f9d793aae56
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-07-19 22:23:26 +0200

    Creating sub classes

--- computer.php
+++ computer.php
@@ -17,3 +17,36 @@ class Computer
         echo "Model: " . $this->model . "<br>";
     }
 }
+class DesktopComputer extends Computer
+{
+    protected $formFactor;
+
+    public function __construct($brand, $model, $formFactor)
+    {
+        parent::__construct($brand, $model);
+        $this->formFactor = $formFactor;
+    }
+
+    public function displayInfo()
+    {
+        parent::displayInfo();
+        echo "Form Factor: " . $this->formFactor . "<br>";
+    }
+}
+
+class LaptopComputer extends Computer
+{
+    protected $batteryLife;
+
+    public function __construct($brand, $model, $batteryLife)
+    {
+        parent::__construct($brand, $model);
+        $this->batteryLife = $batteryLife;
+    }
+
+    public function displayInfo()
+    {
+        parent::displayInfo();
+        echo "Battery Life: " . $this->batteryLife . " hours<br>";
+    }
+}
